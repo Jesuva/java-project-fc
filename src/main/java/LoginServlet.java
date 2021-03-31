c090bee78bd63d64bdae7500a94ca41ebcd82691
Jesuva <jesuva.2308@gmail.com>
2021-03-31 15:44:44 +0530

    added course selection page after login

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -11,37 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("/DWP/index.html");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String password = request.getParameter("userPassword");
 		if(password.equals("user")) {
+			request.setAttribute("name", request.getParameter("userName"));
 			RequestDispatcher rd = request.getRequestDispatcher("/welcome.html");
 			rd.forward(request, response);
+			
 		}
 		else{
 			response.getWriter().print("Invalid User!");
